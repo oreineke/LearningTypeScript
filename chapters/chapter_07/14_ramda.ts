369e05c7e9e8c9f8dabf22d0d1b25fc58da1bd69
remojansen <remo.jansen@wolksoftware.com>
2017-10-24 01:23:35 +0100

    Chapter 07

--- chapters/chapter_07/14_ramda.ts
+++ chapters/chapter_07/14_ramda.ts
@@ -1,3 +1,23 @@
+import * as R from "ramda";
+
+// npm install --save ramda
+// npm install --save-dev @types/ramda
+
 namespace ramda_demo_1 {
-    // TODO
+
+    const trim = (s: string) => s.trim();
+    const capitalize = (s: string) => s.toUpperCase();
+    const trimAndCapitalize = R.compose(trim, capitalize);
+
+    const replace = (s: string, f: string, r: string) =>
+        s.split(f).join(r);
+
+    const curriedReplace = R.curry(replace);
+    const trimCapitalizeAndReplace = R.compose(
+        trimAndCapitalize,
+        curriedReplace("/")("-")
+    );
+
+    trimAndCapitalize("   13/feb/1989   "); // "13-FEB-1989"
+
 }
