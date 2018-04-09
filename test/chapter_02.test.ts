011de49f13e77ad2012a03f2a3c17e2bb3976ad0
Remo H. Jansen <remo.jansen@wolksoftware.com>
2018-04-09 04:05:00 +0100

    WIP test for chapter 02

---
+++ test/chapter_02.test.ts
@@ -0,0 +1,6 @@
+import { shouldNotThrow, shouldThrow } from "./test_utils";
+import * as path from "path";
+
+export function testChapter02() {
+    console.log("TODO");
+}
---
+++ test/index.ts
@@ -0,0 +1,5 @@
+import { testChapter01 } from "./chapter_01.test";
+import { testChapter02 } from "./chapter_02.test";
+
+testChapter01();
+testChapter02();
